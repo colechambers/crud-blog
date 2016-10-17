9102736e0a9016978ae6576538f4545580d2062a
Cole Chambers <cole.chambers@icloud.com>
2016-10-16 21:38:37 -0600

    added migration authors.js

--- migrations/20161016213312_authors.js
+++ migrations/20161016213312_authors.js
@@ -1,8 +1,16 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('authors', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').unique().notNullable();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('authors');
 };
